542eb1136707d6bf56c671126c23162cf00b16c0
Fauzan BerRecode <85762379+BerrRecode@users.noreply.github.com>
2021-11-03 10:10:38 +0700

    update id hexa to fznadmn

--- main.js
+++ main.js
@@ -18,72 +18,72 @@ return new Promise(resolve => setTimeout(resolve, ms))
 require('./index.js')
 nocache('./index.js', module => console.log(`${module} is now updated!`))
 
-const starts = async (hexa = new WAConnection()) => {
-    hexa.logger.level = 'warn'
-    hexa.version = [3, 3234, 9]
-    hexa.browserDescription = [ 'FznAdmn', 'Chrome', '3.0' ]
+const starts = async (fznadmn = new WAConnection()) => {
+    fznadmn.logger.level = 'warn'
+    fznadmn.version = [3, 3234, 9]
+    fznadmn.browserDescription = [ 'FznAdmn', 'Chrome', '3.0' ]
     console.log(banner.string)
-    hexa.on('qr', () => {
+    fznadmn.on('qr', () => {
         console.log(color('[','white'), color('!','red'), color(']','white'), color('Follow ig gw @efzyn_'))
     })
 
-    fs.existsSync('./LiluluBot.json') && hexa.loadAuthInfo('./LiluluBot.json')
-    hexa.on('connecting', () => {
+    fs.existsSync('./LiluluBot.json') && fznadmn.loadAuthInfo('./LiluluBot.json')
+    fznadmn.on('connecting', () => {
         start('2', 'Login...')
     })
-    hexa.on('open', () => {
+    fznadmn.on('open', () => {
         success('2', 'DONE! jangan lupa follow ig gw @efzyn_')
     })
-    await hexa.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./LiluluBot.json', JSON.stringify(hexa.base64EncodedAuthInfo(), null, '\t'))
+    await fznadmn.connect({timeoutMs: 30*1000})
+        fs.writeFileSync('./LiluluBot.json', JSON.stringify(fznadmn.base64EncodedAuthInfo(), null, '\t'))
 
-    hexa.on('chat-update', async (message) => {
-        require('./index.js')(hexa, message)
+    fznadmn.on('chat-update', async (message) => {
+        require('./index.js')(fznadmn, message)
     })
     //=======ini fungsi welcomenya=======//
-     hexa.on('group-participants-update', async (anu) => {
+     fznadmn.on('group-participants-update', async (anu) => {
            mem = anu.participants[0]
-			const mdata = await hexa.groupMetadata(anu.jid)
+			const mdata = await fznadmn.groupMetadata(anu.jid)
 			try {
 			console.log(anu)
 			if (anu.action == 'add') {
 			fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '62838505090133-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;FznAdm;;;\nFN:FznAdm\nitem1.TEL;waid=6285156724122:6285156724122\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 		    num = anu.participants[0]
 			try {
-			ppimg = await hexa.getProfilePicture(`${num.split('@')[0]}@c.us`)
+			ppimg = await fznadmn.getProfilePicture(`${num.split('@')[0]}@c.us`)
 			} catch {
 			ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 			}
 			let buff = await getBuffer(ppimg)
 			welcomimg = fs.readFileSync('./src/welcomimg.jpg')
 			masuk =`Halo @${mem.split('@')[0]}\n*Selamat Datang Di ${mdata.subject}*\n\nSilahkan baca Deskripsi grup dulu ya!\nketik *!menu* untuk menampilkan fitur bot\njangan lupa donasi ya :)`
-            hexa.sendMessage(mdata.id, welcomimg, MessageType.image, {caption: masuk, quoted: fkontakk, thumbnail: fs.readFileSync('./src/welcomimg.jpg')})
+            fznadmn.sendMessage(mdata.id, welcomimg, MessageType.image, {caption: masuk, quoted: fkontakk, thumbnail: fs.readFileSync('./src/welcomimg.jpg')})
 			} else if (anu.action == 'remove') {
 			fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '62838505090133-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;FznAdm;;;\nFN:FznAdm\nitem1.TEL;waid=6285156724122:6285156724122\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 			num = anu.participants[0]
 			try {
-			ppimg = await hexa.getProfilePicture(`${num.split('@')[0]}@c.us`)
+			ppimg = await fznadmn.getProfilePicture(`${num.split('@')[0]}@c.us`)
 			} catch {
 			ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 			}
 			let buff = await getBuffer(ppimg)
 			gdbyimg = fs.readFileSync('./src/byeimg.jpg')
 			keluar =`Selamat tinggal @${mem.split('@')[0]}\nNanti kalo udh balik, jangan lupa bawa oleh-oleh ya`
-            hexa.sendMessage(mdata.id, gdbyimg, MessageType.image, {caption: keluar, quoted: fkontakk, thumbnail: fs.readFileSync('./src/byeimg.jpg')})
+            fznadmn.sendMessage(mdata.id, gdbyimg, MessageType.image, {caption: keluar, quoted: fkontakk, thumbnail: fs.readFileSync('./src/byeimg.jpg')})
 			}
 //===========function promote demote detected============//
 			else if (anu.action == 'promote') {
 fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '62838505090133-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;FznAdmn;;;\nFN:FznAdmn\nitem1.TEL;waid=6285156724122:6285156724122\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 num = anu.participants[0]
 teks = `*P R O M O T E - D E T E C T E D*\n Username: @${num.split('@')[0]}\n Time : ${moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')}\n Group: ${mdata.subject}`
-hexa.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
+fznadmn.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
 console.log(color('|TRM|'), color(`Promote Member ${num.split('@')[0]} In ${mdata.subject}`,  'cyan'))
 } 
 else if (anu.action == 'demote') {
 fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '62838505090133-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${mdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;FznAdmn;;;\nFN:FznAdmn\nitem1.TEL;waid=6285156724122:6285156724122\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
 num = anu.participants[0]
 teks = `*D E M O T E - D E T E C T E D*\n Username: @${num.split('@')[0]}\n Time : ${moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')}\n Group: ${mdata.subject}`
-hexa.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
+fznadmn.sendMessage(mdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [num]}, quoted: fkontakk})
 console.log(color('|TRM|'), color(`Demote Admin ${num.split('@')[0]} In ${mdata.subject}`,  'cyan'))
 }
 		    } catch (e) {
@@ -92,26 +92,26 @@ console.log(color('|TRM|'), color(`Demote Admin ${num.split('@')[0]} In ${mdata.
 	        })	     
 //==========function call auto block=============//
 //ambil di bitcbot
-hexa.on('CB:action,,call', async json => {
+fznadmn.on('CB:action,,call', async json => {
         const callerId = json[2][0][1].from;
         var vcard = 'BEGIN:VCARD\n' + 'VERSION:3.0\n' + 'FN:' + `FznAdmn` + '\n' + `ORG:Developer Lilulu-BOT\n` + 'TEL;type=CELL;type=VOICE;waid=' + `6285156724122` + ':+' + `6285156724122` + '\n' + 'END:VCARD'
-        hexa.sendMessage(callerId, "\`\`\`[ ! ] CALL DETECTED [ ! ]\`\`\`\n\n\`\`\`Maaf Anda Di Block Karena Telepon Bot , Silahkan Hubungi Developer Bot Untuk Membuka Block\`\`\`", MessageType.text)
-        hexa.sendMessage(callerId, { displayname: `FznAdmn`, vcard: vcard}, MessageType.contact, {contextInfo: { externalAdReply:{title: `Developer Lilulu-BOT`,body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./stik/thumb.jpeg'),sourceUrl:`https://wa.me/6285156724122?text=Assalamualaikum`}}})
+        fznadmn.sendMessage(callerId, "\`\`\`[ ! ] CALL DETECTED [ ! ]\`\`\`\n\n\`\`\`Maaf Anda Di Block Karena Telepon Bot , Silahkan Hubungi Developer Bot Untuk Membuka Block\`\`\`", MessageType.text)
+        fznadmn.sendMessage(callerId, { displayname: `FznAdmn`, vcard: vcard}, MessageType.contact, {contextInfo: { externalAdReply:{title: `Developer Lilulu-BOT`,body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./stik/thumb.jpeg'),sourceUrl:`https://wa.me/6285156724122?text=Assalamualaikum`}}})
         await sleep(5000)
-        await hexa.blockUser(callerId, "add")
+        await fznadmn.blockUser(callerId, "add")
         })
 //===============function anti delete=================//
 //ambil di bitcbot
 /*
-hexa.on('message-delete', async (mek) => {
+fznadmn.on('message-delete', async (mek) => {
 if (mek.key.remoteJid == 'status@broadcast') return
 if (!mek.key.fromMe) {
 mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 const jam = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 let d = new Date
-let c = hexa.chats.get(mek.key.remoteJid)
+let c = fznadmn.chats.get(mek.key.remoteJid)
 let a = c.messages.dict[`${mek.key.id}|${mek.key.fromMe ? 1 : 0}`]
-let co3ntent = hexa.generateForwardMessageContent(a, false)
+let co3ntent = fznadmn.generateForwardMessageContent(a, false)
 let c3type = Object.keys(co3ntent)[0]
 let locale = 'id'
 let gmt = new Date(0).getTime() - new Date('1 Januari 2021').getTime()
@@ -122,8 +122,8 @@ day: 'numeric',
 month: 'long',
 year: 'numeric'
 })
-hexa.copyNForward(mek.key.remoteJid, mek.message)
-hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
+fznadmn.copyNForward(mek.key.remoteJid, mek.message)
+fznadmn.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 
 ▢ \`\`\`Nama : @${mek.participant.split("@")[0]}\`\`\`
 ▢ \`\`\`Tipe : ${c3type}\`\`\`
@@ -132,14 +132,14 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 })
 
 //=================Anti Delete=================//
-	hexa.on('message-delete', async (mek) => {
+	fznadmn.on('message-delete', async (mek) => {
 		try {
 	    const from = mek.key.remoteJid
 		const messageStubType = WA_MESSAGE_STUB_TYPES[mek.messageStubType] || 'MESSAGE'
 		const dataRevoke = JSON.parse(fs.readFileSync('./src/gc-revoked.json'))
 		const dataCtRevoke = JSON.parse(fs.readFileSync('./src/ct-revoked.json'))
 		const dataBanCtRevoke = JSON.parse(fs.readFileSync('./src/ct-revoked-banlist.json'))
-		const sender = mek.key.fromMe ? hexa.user.jid : mek.key.remoteJid.endsWith('@g.us') ? mek.participant : mek.key.remoteJid
+		const sender = mek.key.fromMe ? fznadmn.user.jid : mek.key.remoteJid.endsWith('@g.us') ? mek.participant : mek.key.remoteJid
 		const isRevoke = mek.key.remoteJid.endsWith('@s.whatsapp.net') ? true : mek.key.remoteJid.endsWith('@g.us') ? dataRevoke.includes(from) : false
 		const isCtRevoke = mek.key.remoteJid.endsWith('@g.us') ? true : dataCtRevoke.data ? true : false
 		const isBanCtRevoke = mek.key.remoteJid.endsWith('@g.us') ? true : !dataBanCtRevoke.includes(sender) ? true : false
@@ -153,8 +153,8 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 			let int
 			let infoMSG = JSON.parse(fs.readFileSync('./src/msg.data.json'))
 			const id_deleted = mek.key.id
-			const conts = mek.key.fromMe ? hexa.user.jid : hexa.contacts[sender] || { notify: jid.replace(/@.+/, '') }
-			const pushname = mek.key.fromMe ? hexa.user.name : conts.notify || conts.vname || conts.name || '-'
+			const conts = mek.key.fromMe ? fznadmn.user.jid : fznadmn.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+			const pushname = mek.key.fromMe ? fznadmn.user.name : conts.notify || conts.vname || conts.name || '-'
 			const opt4tag = {
 				contextInfo: { mentionedJid: [sender] }
 			}
@@ -195,7 +195,7 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 - Waktu : ${moment.unix(int.timestamp).format('HH:mm:ss')}
 - Tanggal : ${moment.unix(int.timestamp).format('DD/MM/YYYY')}
 - Pesan : ${body ? body : '-'}`
-				hexa.sendMessage(from, strConversation, MessageType.text, selepbot72)
+				fznadmn.sendMessage(from, strConversation, MessageType.text, selepbot72)
 			} else if (int.type == 'stickerMessage') {
 				var itsme = `0@s.whatsapp.net`
 					var split = `${fake}`
@@ -210,7 +210,7 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 						}
 					}
 				const filename = `${sender.replace('@s.whatsapp.net', '')}-${moment().unix()}`
-				const savedFilename = await hexa.downloadAndSaveMediaMessage(int.data, `./media/sticker/${filename}`)
+				const savedFilename = await fznadmn.downloadAndSaveMediaMessage(int.data, `./media/sticker/${filename}`)
 				const strConversation = `		 「 ANTI-DELETE 」
 
 - Nama : ${pushname} 
@@ -220,8 +220,8 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 - Tanggal : ${moment.unix(int.timestamp).format('DD/MM/YYYY')}`
 
 				const buff = fs.readFileSync(savedFilename)
-				hexa.sendMessage(from, strConversation, MessageType.text, opt4tag)
-				hexa.sendMessage(from, buff, MessageType.sticker, pingbro23)
+				fznadmn.sendMessage(from, strConversation, MessageType.text, opt4tag)
+				fznadmn.sendMessage(from, buff, MessageType.sticker, pingbro23)
 				fs.unlinkSync(savedFilename)
 
 			} else if (int.type == 'imageMessage') {
@@ -238,7 +238,7 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 						}
 					}
 				const filename = `${sender.replace('@s.whatsapp.net', '')}-${moment().unix()}`
-				const savedFilename = await hexa.downloadAndSaveMediaMessage(int.data, `./media/revoke/${filename}`)
+				const savedFilename = await fznadmn.downloadAndSaveMediaMessage(int.data, `./media/revoke/${filename}`)
 				const buff = fs.readFileSync(savedFilename)
 				const strConversation = `	 「 ANTI-DELETE 」
 
@@ -248,7 +248,7 @@ hexa.sendMessage(mek.key.remoteJid, `▷\`\`\`Anti Delete\`\`\`
 - Waktu : ${moment.unix(int.timestamp).format('HH:mm:ss')}
 - Tanggal : ${moment.unix(int.timestamp).format('DD/MM/YYYY')}
 - Pesan : ${body ? body : '-'}\`\`\``
-				hexa.sendMessage(from, buff, MessageType.image, { contextInfo: { mentionedJid: [sender] }, caption: strConversation })
+				fznadmn.sendMessage(from, buff, MessageType.image, { contextInfo: { mentionedJid: [sender] }, caption: strConversation })
 				fs.unlinkSync(savedFilename)
 			}
 		}
